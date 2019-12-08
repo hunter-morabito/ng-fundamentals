9fe8d831a6dcafaef180234e96717f337b8e8140
Hunter Morabito <hunter.morabito@gmail.com>
2019-12-08 14:44:30 -0500

    using template variables

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core'
+import {Component, Input, Output, EventEmitter} from '@angular/core'
 
 @Component({
     selector: 'event-thumbnail',
@@ -17,4 +17,9 @@ import {Component, Input} from '@angular/core'
 })
 export class EventThumbnailComponent{
     @Input() event:any
+    someProperty:any = "some property"
+
+    logFoo(){
+        console.log('foo')
+    }
 }
