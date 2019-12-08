bcae0ce04b7f8c986d1d61ae27c73158e4a6becb
Hunter Morabito <hunter.morabito@gmail.com>
2019-12-08 16:45:58 -0500

    hiding content with property binding

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -9,9 +9,9 @@ import {Component, Input, Output, EventEmitter} from '@angular/core'
             <div>Price: \${{event?.price}}</div>
             <div *ngIf="event?.location">
                 <span>Location: {{event?.location?.address}}</span>
-                <span class="pad-left">{{event?.location?.city}}, {{event?.location.country}}</span>
+                <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
             </div>
-            <div *ngIf="event?.onlineUrl">
+            <div [hidden]="!event?.onlineUrl">
                 Online URL: {{event?.onlineUrl}}
             </div>
         </div>
