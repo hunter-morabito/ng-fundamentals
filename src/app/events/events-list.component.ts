f013e78d5c7b503a97649a15aad54ae4330d56d8
xzm9ryv <hmorabito@ups.com>
2019-12-10 20:30:34 -0500

    Wrapping third party services

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
+import { ToastrService } from '../common/toastr.service';
 
+/* Lets ts know toastr is in scope through angular.json */
 @Component({
     selector: 'events-list',
     template: `
@@ -9,20 +11,25 @@ import { EventService } from './shared/event.service';
         <hr/>
         <div class="row">
             <div *ngFor="let event of events" class="col-md-5">
-                <event-thumbnail [event] ="event"></event-thumbnail>
+                <event-thumbnail (click)="handleThumbnailClick(event.name)" [event] ="event"></event-thumbnail>
             </div>
         </div>
     </div>
     `
 })
 export class EventsListComponent implements OnInit{
-  events:any[]
+  events: any[];
   /* short hand for declaring and setting a variable */
-  constructor(private eventService: EventService){
+  constructor(private eventService: EventService,
+              private toastr: ToastrService){
   }
 
   /* Lifecycle hook */
   ngOnInit() {
     this.events = this.eventService.getEvents();
+  }
+
+  handleThumbnailClick(eventName){
+    this.toastr.success(eventName);
   }
 }
